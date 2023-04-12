4f81d42463f0425ac1ecd0149e434ef6c307e472
eh19257 <eh19257@bristol.ac.uk>
2023-04-12 17:05:32 +0100

    increasing the number of nodes

--- models/run_model.sh
+++ models/run_model.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name bp_model_2_bit_tf_transformer_pwd
 #SBATCH --partition gpu
-#SBATCH --nodes 1
-#SBATCH --gres gpu:1
+#SBATCH --nodes 2
+#SBATCH --gres gpu:2
 #SBATCH --mem 48GB
 #SBATCH --account=COSC027924
 #SBATCH -o ./outputs/log_%j.out
