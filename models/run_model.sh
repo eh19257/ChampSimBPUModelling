2d701fac9c075e77c7c9081ae7f184212130542e
eh19257 <eh19257@bristol.ac.uk>
2023-04-12 15:39:11 +0100

    fixin batch file

--- models/run_model.sh
+++ models/run_model.sh
@@ -10,15 +10,13 @@
 #SBATCH -e ./outputs/log_%j.err
 
 
-cd "${SLURM_SUBMIT_DIR}"
+echo The current slurm directory is: "${SLURM_SUBMIT_DIR}"
 
-echo "${SLURM_SUBMIT_DIR}"
+# Load tensorflow
+module load lang/python/anaconda/3.9.7-2021.12-tensorflow.2.7.0
+module load lang/cuda/11.2-cudnn-8.1
 
-echo Running on host "$(hostname)"
-echo Time is "$(date)"
-echo Directory is "$(pwd)"
-echo Slurm job ID is "${SLURM_JOBID}"
-echo This jobs runs on the following machines:
-echo "${SLURM_JOB_NODELIST}"
+# Load libcudart.so.11.0
+export LD_LIBRARY_PATH=:$LD_LIBRARY_PATH:/sw/lang/cuda_11.2.2/targets/x86_64-linux/lib
 
 python3 2_bit_model_tf_transformer.py ../../data/bp_models/2_bit/pwd.bin ../../data/bp_models/2_bit/pwd.bin "SAVED_MODEL_tf_transformer"
