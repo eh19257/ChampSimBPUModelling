12bd68cbcda0064350e63df89590f55f54223ec0
eh19257 <eh19257@bristol.ac.uk>
2023-04-16 16:10:04 +0100

    We now have a working data generator - time to enable shuffling!

--- models/2_bit_model_tf_transformer.py
+++ models/2_bit_model_tf_transformer.py
@@ -32,7 +32,7 @@ def read_data(filename):
 
     global Np
     Np = len(raw_data)
-    Np = 10000
+    #Np = 1000
 
     data = np.zeros((Np, 1, 4), dtype=np.double )
     hot_ones = np.zeros((Np, 1, 2), dtype=np.double)
@@ -85,9 +85,11 @@ def positional_encoding(length, depth):
 
 # Used to encode/embed positional data into each input - check out applied deep learning week 8 lecture
 class PositionalEmbedding(tf.keras.layers.Layer):
-  def __init__(self, vocab_size, d_dims):
+  def __init__(self, vocab_size, d_dims, start=0):
     super().__init__()
     self.d_dims = d_dims
+    self.start = start
+    self.vocab_size = vocab_size
     #self.embedding = tf.keras.layers.Embedding(vocab_size, d_dims, mask_zero=True)
     self.expand = layers.Dense(d_dims)
     self.pos_encoding = positional_encoding(length=2048, depth=d_dims)#depth=4)
@@ -96,7 +98,7 @@ class PositionalEmbedding(tf.keras.layers.Layer):
     #return self.embedding.compute_mask(*args, **kwargs)
 
   def call(self, x):
-    length = x.shape[1]#tf.shape(x)[1]
+    length = self.vocab_size#tf.shape(x)[1]
     #x = self.embedding(x)
     # This factor sets the relative scale of the embedding and positonal_encoding.
     x *= tf.math.sqrt(tf.cast(self.d_dims, tf.float32))
@@ -109,7 +111,7 @@ class PositionalEmbedding(tf.keras.layers.Layer):
     #x = x + self.pos_encoding[tf.newaxis, :length, :]
     #tf.print("LENGTH:", length)
 
-    x = x + self.pos_encoding[tf.newaxis, :length, :]
+    x = x + self.pos_encoding[tf.newaxis, self.start : self.start + length, :]
     return x
 
 
@@ -222,8 +224,8 @@ class EncoderLayer(layers.Layer):
         )
     
     def call(self, x):
-        x = self.self_attention(x)
-        x = self.ff(x)
+        x = self.self_attention(x); print("POST SELF ATTENTION:", x.shape)
+        x = self.ff(x); print("POST FF:", x.shape)
 
         return x
 
@@ -249,11 +251,11 @@ class Encoder(layers.Layer):
         ]
 
     def call(self, x):
-        x = self.pos_embedding(x)
-        x = self.dropout(x)
+        x = self.pos_embedding(x); print("POST POS_EMBEDDING:", x.shape)
+        x = self.dropout(x); print("POST DROPOUT:", x.shape)
 
         for i in range(len(self.enc_layers)):
-            x = self.enc_layers[i](x)
+            x = self.enc_layers[i](x); print("POST ENC_LAYERS:", x.shape)
         return x
 
 
@@ -287,14 +289,15 @@ class DecoderLayer(layers.Layer):
         return x
 
 class Decoder(layers.Layer):
-    def __init__(self, *, num_layers, d_dims, num_heads, ff_fc, vocab_size, dropout_rate=0.1):
+    def __init__(self, *, num_layers, d_dims, num_heads, ff_fc, vocab_size, dropout_rate=0.1, enc_vocab_size=0):
         super(Decoder, self).__init__()
 
         self.d_dims = d_dims
         self.num_layers = num_layers
 
         self.pos_embedding = PositionalEmbedding(vocab_size=vocab_size,
-                                                d_dims=d_dims
+                                                d_dims=d_dims,
+                                                start=enc_vocab_size
         )
 
         self.dropout = layers.Dropout(dropout_rate)
@@ -311,11 +314,11 @@ class Decoder(layers.Layer):
     
 
     def call(self, x, context):
-        x = self.pos_embedding(x)
-        x = self.dropout(x)
+        x = self.pos_embedding(x); print("DEC POST POS_EMBED:", x.shape)
+        x = self.dropout(x); print("DEC POST DROPOUT:", x.shape)
 
         for i in range(self.num_layers):
-            x = self.dec_layers[i](x, context)
+            x = self.dec_layers[i](x, context); print("DEC POST DEC_LAYERS:", x.shape)
 
         self.last_attn_scores = self.dec_layers[-1].last_attn_scores
         
@@ -338,14 +341,15 @@ class Transformer(keras.Model):
                             num_heads=num_heads,
                             ff_fc=ff_fc,
                             vocab_size=target_vocab_size,
-                            dropout_rate=dropout_rate)
+                            dropout_rate=dropout_rate,
+                            enc_vocab_size=input_vocab_size)
         
-        self.reshape = layers.Reshape((1, 1, d_dims),
-                                      input_shape=(1, 1, 4, 256))
+        self.reshape = layers.Reshape((None, 1, d_dims))
+                                      #input_shape=(1, 1, 4, 256))
 
         self.final_fc_1 = layers.Dense(int(d_dims / 32))
 
-        self.final_fc_2 = layers.Dense(target_vocab_size)
+        self.final_fc_2 = layers.Dense(2)
 
         self.softmax = layers.Softmax()
     
@@ -405,30 +409,30 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
 
 def masked_loss(label, pred):
-  mask = label != 0
-  loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none')
-  loss = loss_object(label, pred)
+    mask = label != 0
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+    loss = loss_object(label, pred)
 
-  mask = tf.cast(mask, dtype=loss.dtype)
-  loss *= mask
+    mask = tf.cast(mask, dtype=loss.dtype)
+    loss *= mask
 
-  loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
-  return loss
+    loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+    return loss
 
 
 def masked_accuracy(label, pred):
-  pred = tf.argmax(pred, axis=2)
-  label = tf.cast(label, pred.dtype)
-  match = label == pred
+    pred = tf.argmax(pred, axis=2)
+    label = tf.cast(label, pred.dtype)
+    match = label == pred
 
-  mask = label != 0
+    mask = label != 0
 
-  match = match & mask
+    match = match & mask
 
-  match = tf.cast(match, dtype=tf.float32)
-  mask = tf.cast(mask, dtype=tf.float32)
-  return tf.reduce_sum(match)/tf.reduce_sum(mask)
+    match = tf.cast(match, dtype=tf.float32)
+    mask = tf.cast(mask, dtype=tf.float32)
+    return tf.reduce_sum(match)/tf.reduce_sum(mask)
 
 
 def make_batches(x, y, h=128):
@@ -464,16 +468,24 @@ def make_batches(x, y, h=128):
 
 
 class DataGenerator(keras.utils.Sequence):
-    def __init__(self, x_set, y_set, batch_size):
-        self.x, self.y = x_set, y_set
+    def __init__(self, x_set, y_set, batch_size, h):
+        self.x = x_set
+        self.y = y_set
+
+        self.Np = len(x_set) - h - 1
         self.batch_size = batch_size
 
+        self.h = h
+        #self.dec_dims = dec_dims
+
     def __len__(self):
-        return len(self.x[0]) // self.batch_size#int(np.floor(len(self.x[0]) / float(self.batch_size)))
+        return int(np.floor( self.Np / float(self.batch_size) ) )
+        #return int(np.floor( (len(self.x) - (self.h + 1) ) / float(self.batch_size)))
+
 
     # This should return a single batch
     def __getitem__(self, idx):
-
+        '''
         #tf.print("look at self.x:", self.x)
         enc = self.x[ 0 ][ idx * self.batch_size : (idx + 1) * self.batch_size ]#.reshape((self.batch_size, ))
         dec = self.x[ 1 ][ idx * self.batch_size : (idx + 1) * self.batch_size ]
@@ -482,19 +494,48 @@ class DataGenerator(keras.utils.Sequence):
         #batch_x = (self.x[0][idx * self.batch_size : (idx + 1) * self.batch_size], self.x[1][idx * self.batch_size : (idx + 1) * self.batch_size])
         #batch_y = self.y[idx * self.batch_size : (idx + 1) * self.batch_size]
 
+        enc = enc.reshape( (self.batch_size, self.enc_dims, 4) )
+        dec = dec.reshape( (self.batch_size, self.dec_dims, 4) )
 
-        print("IDX is:", idx)
-        print("Shape of enc", enc.shape)
-        print("Shape of dec", dec.shape)
-        print("")
+        label = label.reshape( (self.batch_size, 1, 2) )
         '''
-        batch_x = []
-        batch_y = []
-        for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
-            batch_x.append( (self.x[0][i], self.x[1][i]) )
-            batch_y.append(  self.y[i] )
+
+        x = self.__gen_x(idx)
+        y = self.__gen_y(idx)
+    
+        return x, y
+
+
+    def __gen_x(self, idx):
+        enc   = np.zeros( ( self.batch_size, self.h, 4), dtype=np.double)
+        dec   = np.zeros( ( self.batch_size, 1     , 4), dtype=np.double)
+
+        for i in range(self.batch_size):
+            enc[i] = self.x[ idx * self.batch_size + i          : idx * self.batch_size + i + self.h    ].reshape( (self.h, 4) )
+            enc[i] = self.x[ idx * self.batch_size + i + self.h : idx * self.batch_size + i + self.h + 1].reshape( (1     , 4) )
+
+            dec[i][0][3] = 0.5
+        
         '''
-        return (enc, dec), label
+        for i in range(1, self.batch_size + 1):
+            enc[i] = self.x[ i * idx          : i + self.h     ].reshape( (self.h, 4) )
+            dec[i] = self.x[ i + self.h : i + self.h + 1 ].reshape( (1     , 4) )
+
+            # Edit the actual_branch_behaviour of dec so that it's 0.5 (i.e. a probability that could be either taken or not taken)  
+            dec[i][0][3] = 0.5
+        '''
+        
+        return (enc, dec)
+
+
+    def __gen_y(self, idx):
+        y_out = np.zeros( ( self.batch_size, 1, 2), dtype=np.double)
+
+        for i in range(self.batch_size):
+            y_out[i] = self.y[ idx * self.batch_size + i + self.h : idx * self.batch_size + i + self.h + 1 ].reshape((1, 2))
+        
+        return y_out
+
 
 ###################################################################################################
 
@@ -505,7 +546,7 @@ num_heads = 4
 dropout_rate = 0.1
 HISTORY_TABLE_SIZE = 128
 
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 #BUFFER_SIZE = 1000 # The number of elements and NOT the number of bytes for the buffer
 
 
@@ -515,7 +556,7 @@ transformer = Transformer(
     num_heads=num_heads,
     ff_fc=ff_fc,
     input_vocab_size=HISTORY_TABLE_SIZE,
-    target_vocab_size=2,
+    target_vocab_size=1,
     dropout_rate=dropout_rate)
 
 
@@ -533,31 +574,35 @@ x_train_raw, y_train_raw = read_data(sys.argv[1])
 
 print("Size of x_train_raw:", x_train_raw.shape)
 
-x_train, y_train = make_batches(x_train_raw, y_train_raw, h=HISTORY_TABLE_SIZE)
+#x_train, y_train = make_batches(x_train_raw, y_train_raw, h=HISTORY_TABLE_SIZE)
 #x_test,  y_test  = make_batches(x_test_raw,  y_test_raw, h=HISTORY_TABLE_SIZE)
 
 print("Data is being converted into a data generator...")
 
 #train = tf.data.Dataset.from_generator(DataGenerator(x_train, y_train, BATCH_SIZE))
-train = DataGenerator(x_train, y_train, BATCH_SIZE)
+train = DataGenerator(x_train_raw, y_train_raw, BATCH_SIZE, HISTORY_TABLE_SIZE)
 #test  = DataGenerator(x_test, y_test, BATCH_SIZE)
 
 
 #print("Example Line:", train.__getitem__(1))
-
-#for i in train: print(i)
-transformer(train.__getitem__(0)[0])
-transformer.summary()
+(foo, bar), baz = train.__getitem__(1)
+print("enc:", foo.shape, ". Dec:", bar.shape, ". Labels:", baz.shape, ".")
 
 
+#for i in train: print("enc:", i[0][0].shape, ". Dec:", i[0][1].shape, ". Labels:", i[1].shape, ".")
+#transformer(train.__getitem__(0)[0])
+#transformer.summary()
+
+print("STARTING TRAINING!!!")
 transformer.fit(
     train,
     #x=x_train,
     #y=y_train, 
     epochs=1,
     #batch_size=BATCH_SIZE,
-    shuffle=False#,
+    shuffle=False
     #validation_data=test
+    #verbose='auto'
 )
 '''
 transformer.save(sys.argv[3])
