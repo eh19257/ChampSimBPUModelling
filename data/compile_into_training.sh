4aa995c47b0a9743ed048164aafbfa5d91a2fb7b
Karl Sachs <eh19257@bristol.ac.uk>
2023-04-19 03:33:43 +0100

    fixin scripts

--- data/compile_into_training.sh
+++ data/compile_into_training.sh
@@ -1,9 +1,9 @@
 # EDIT the variable BP here - then run the program
 export BP="BT_FNT"
 
-cat ./bp_models/${BP}/pwd.bin > train.bin
-cat ./bp_models/${BP}/557_XZ.bin >> train.bin
-cat ./bp_models/${BP}/531.DEEPSJENG.bin >> train.bin
-cat ./bp_models/${BP}/523.XALANCBMK.bin >> train.bin
+cat ./bp_models/${BP}/pwd.bin > ./bp_models/${BP}/train.bin
+cat ./bp_models/${BP}/557_XZ.bin >> ./bp_models/${BP}/train.bin
+cat ./bp_models/${BP}/531.DEEPSJENG.bin >> ./bp_models/${BP}/train.bin
+cat ./bp_models/${BP}/523.XALANCBMK.bin >> ./bp_models/${BP}/train.bin
 
 echo "./bp_models/${BP}/train.bin has been generated"
